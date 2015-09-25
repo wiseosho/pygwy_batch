c5abe88a0a4f5f9551ca2f4d71a3ba94c5592bd8
JYSon <wiseosho@gmail.com>
2015-09-25 17:37:19 +0900

    merged 0921_Iteration.py Existed Features 1. Merged opened files into one gwy file 2. preprocess(flatten, linecorrection, remove scars) 3. change export settings( palette change/gold/, colorrange auto) 4. export as png files
    Added Features
    	1. Load files from a directory and make current and visible

--- scripts/Pygwycons_final.py
+++ scripts/Pygwycons_final.py
@@ -1,7 +1,20 @@
-import gwyutils, gwy
-filename_save = "/home/june/pygwy/test.gwy"
-#Get all list of Loaded Containers(Opened Files)
-OpenedFiles_All = gwy.gwy_app_data_browser_get_containers()
+import gwyutils, gwy, gc
+from copy import deepcopy
+from os import listdir, mkdir, getcwd
+from os.path import isfile, join
+#cwd = '/home/june/바탕화면/5hr10t reexp'
+#cwd = '/home/june/바탕화면/Pragati-R/R1/12-09-2013 R1 n R2'
+cwd = '/home/june/pygwy/Ring'
+Files_To_Open = [ f for f in listdir(cwd) if isfile(join(cwd,f)) ]
+try:
+	mkdir(join(cwd,'Processed'))
+
+except Exception as sym:
+	print ('Already Exist')
+Tobe_Saved = join(cwd, 'Processed')
+filename_save = cwd.split('/')[-1]
+print 
+
 # Export PNG with scalebar
 s = gwy.gwy_app_settings_get()
 s['/module/pixmap/title_type'] = 0
@@ -9,51 +22,100 @@ s['/module/pixmap/ztype'] = 0
 s['/module/pixmap/xytype'] = 0
 s['/module/pixmap/draw_maskkey'] = False
 # ... (*lots* of possible settings, see ~/.gwyddion/settings)
+print (Files_To_Open)
+#Load first file to use as Merged file
+for filename in Files_To_Open:
+	print(filename)
+	try:
+		print('try')
+		Temp = gwy.gwy_file_load(join(cwd,filename), RUN_NONINTERACTIVE)
+		print(type(Temp))
+		if type(Temp) == gwy.Container :
+			print('right type')
+			Cont_Dest = Temp
+			Files_To_Open.remove(filename)
+			print (loaded)
+			break
+		Files_To_Open.remove(filename)
+		print('loadedbutnot')
+	except Exception as sym:
+		print('except')
+		print ("not proper file"+str(sym)+"\n")
+		continue
 
-#Get filename of the container & Datafield Titles
-Filename = []
-Filename = OpenedFiles_All[0].get_string_by_name('/filename').split('/')[-1]
+
+gwy.gwy_app_data_browser_add(Cont_Dest)
+#Make Visible
+Cont_Dest.set_boolean_by_name('/0/data/visible', 1)
+print (Files_To_Open)
 #File Merge
 #First Container
-DataFields = gwyutils.get_data_fields_dir(OpenedFiles_All[0])
+DataFields = gwyutils.get_data_fields_dir(Cont_Dest)
 for key in DataFields.keys():
-	title = OpenedFiles_All[0].get_string_by_name(key+"/title")
-	if (title == 'Amplitude') : OpenedFiles_All[0].remove_by_prefix('/'+key.split('/')[1]+'/')
-	OpenedFiles_All[0].set_string_by_name(key+'/title', title+'.'+Filename)
+	title = Cont_Dest.get_string_by_name(key+"/title")
+	if (title == 'Amplitude') : Cont_Dest.remove_by_prefix('/'+key.split('/')[1]+'/')
+	Cont_Dest.set_string_by_name(key+'/title', title+'.'+Files_To_Open[0])
+
 #Rest of Containers
-for OpenedFile in OpenedFiles_All[1:]:
-	Filename = OpenedFile.get_string_by_name('/filename').split('/')[-1]
-	DataFields = gwyutils.get_data_fields_dir(OpenedFile)
+for filename in Files_To_Open :
+	#print (orgfile, join(cwd,filename))
+	try:
+		Temp_Source = gwy.gwy_file_load(join(cwd,filename), RUN_NONINTERACTIVE)
+		if type(Temp_Source) == gwy.Container:
+			Cont_Source = Temp_Source
+			pass
+		else:
+			continue
+	except Exception as sym:
+		print ("not proper file"+sym+"\n")
+		continue
+	DataFields = gwyutils.get_data_fields_dir(Cont_Source)
 	for key in DataFields.keys():
 		ID = key.split('/')[1]
-		title = OpenedFile.get_string_by_name(key+"/title")
+		title = Cont_Source.get_string_by_name(key+"/title")
 		if (title == 'Height') :
-			OpenedFile.set_string_by_name(key+'/title', title+'.'+Filename)
-			gwy.gwy_app_data_browser_copy_channel(OpenedFile, int(ID), OpenedFiles_All[0])
+			Cont_Source.set_string_by_name(key+'/title', title+'.'+filename)
+			gwy.gwy_app_data_browser_copy_channel(Cont_Source, int(ID), Cont_Dest)
 			print (key, title)
+	gwy_app_data_browser_remove(Cont_Source)
+	del Cont_Source
+	print (gc.collect())
+
 
 #Change Palette, Flatten, Correct line, Remove Scars, Change Scale
-OpenedFile = OpenedFiles_All[0]
-DataFields = gwyutils.get_data_fields_dir(OpenedFile)
+DataFields = gwyutils.get_data_fields_dir(Cont_Dest)
 for key in DataFields.keys():
 	ID = key.split('/')[1]
-	title = OpenedFile.get_string_by_name(key+"/title")
+	title = Cont_Dest.get_string_by_name(key+"/title")
 	print (title+'\n')
 	# Subtract polynomial background
 	coeffs = DataFields[key].fit_polynom(3, 3)
 	DataFields[key].subtract_polynom(3, 3, coeffs)
 	DataFields[key].data_changed()
+	#Get X Y scale
+	si = {'x' : 'um' , 'y' : 'um'}
+	size_x = DataFields[key].get_xreal()*1000000
+	if (size_x < 1.0):
+		size_x = size_x * 1000
+		si['x'] = 'nm'
+	size_y = DataFields[key].get_yreal()*1000000
+	if (size_y < 1.0):
+		size_y = size_y * 1000
+		si['y'] = 'nm'
+	scale = str(size_x)+si['x']+'by'+str(size_y)+si['y']
+	title = title + '_'+ scale
 	# Line and scar correction (run module functions)
-	gwy.gwy_app_data_browser_select_data_field(OpenedFile, int(ID))
-	gwy.gwy_process_func_run("line_correct_median", OpenedFile, gwy.RUN_IMMEDIATE)
-	gwy.gwy_process_func_run("scars_remove", OpenedFile, gwy.RUN_IMMEDIATE)
-	gwy.gwy_process_func_run("fix_zero", OpenedFile, gwy.RUN_IMMEDIATE)
+	gwy.gwy_app_data_browser_select_data_field(Cont_Dest, int(ID))
+	gwy.gwy_process_func_run("line_correct_median", Cont_Dest, gwy.RUN_IMMEDIATE)
+	gwy.gwy_process_func_run("scars_remove", Cont_Dest, gwy.RUN_IMMEDIATE)
+	gwy.gwy_process_func_run("fix_zero", Cont_Dest, gwy.RUN_IMMEDIATE)
 	#Get Color Type
-	colorr = OpenedFile.get_int32_by_name('/'+ID+'/base/range-type')
+	colorr = Cont_Dest.get_int32_by_name('/'+ID+'/base/range-type')
 	#Change_Color Palette
-	OpenedFile.set_string_by_name('/'+ID+'/base/palette', 'Gold')
+	Cont_Dest.set_string_by_name('/'+ID+'/base/palette', 'Gold')
 	#Change Color Range into (Full:0, Manual:1, Auto:2, Adaptive:3)
-	OpenedFile.set_int32_by_name('/'+ID+'/base/range-type', 2)
-	gwy.gwy_file_save(OpenedFile, '/home/june/pygwy/test%d.png' % int(ID), gwy.RUN_NONINTERACTIVE)
-gwy.gwy_file_save(OpenedFiles_All[0],filename_save, gwy.RUN_NONINTERACTIVE)
-
+	Cont_Dest.set_int32_by_name('/'+ID+'/base/range-type', 2)
+	print (title)
+	gwy.gwy_file_save(Cont_Dest, Tobe_Saved+'/'+str(title)+'%d.png' % int(ID), gwy.RUN_NONINTERACTIVE)
+	Cont_Dest.set_boolean_by_name('/'+ID+'/data/visible', 0)
+gwy.gwy_file_save(Cont_Dest,Tobe_Saved+'/'+filename_save+'.gwy', gwy.RUN_NONINTERACTIVE)
