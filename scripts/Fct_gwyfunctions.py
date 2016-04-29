6bab11ccddf2009378ccfa8394c3f883bc1838b2
JYSon <wiseosho@gmail.com>
2016-04-30 02:29:56 +0900

    Intermittant commit on 20160429

--- scripts/Fct_gwyfunctions.py
+++ scripts/Fct_gwyfunctions.py
@@ -1,24 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from os import listdir, mkdir
+from os.path import isfile, join, isdir
+import os, datetime, shutil
 import gwyutils, gwy, gc
 from copy import deepcopy
 from os import listdir, mkdir, getcwd
 from os.path import isfile, join, isdir
 import os, datetime
-import re, shutil, imp
+import re, shutil
 import numpy as np
-if not '/home/june/.gwyddion/pygwy/scripts' in sys.path:
-	sys.path.append('/home/june/.gwyddion/pygwy/scripts')
-import Fct_gwyfunctions as custgwy
-imp.reload(custgwy)
-#ratio for color range
-ratio = (0.05, 0.99)
 
-pngexp = '/home/june/Dropbox/AFMImages'
-cwd = '/home/june/LabServer/24HC/Images/SS-Canvas/SSC(T)/151108'
-root = '/home/june/LabServer/24HC/Images/SS-Canvas/SSC(T)/151108'
-cwd = '/home/june/LabServer/08JY/Exp.Data/sscanvas-GB/origami-1/20140511'
-root = '/home/june/LabServer/08JY/Exp.Data/sscanvas-GB/origami-1/20140511'
+def sep_files(root, cwd, pdr):
+	dirs = [ f for f in listdir(cwd) if isdir(join(cwd,f)) ]
+	#dirs = {'./' : Files}
+	for dr in dirs:
+		sep_files(root, join(cwd, dr), dr)
+	time = {}
+	#########File Seperation(for non remaining directories)################
+	Files_to_open = [ f for f in listdir(cwd) if isfile(join(cwd, f)) ]
+	for file in Files_to_open:
+		mtime = os.path.getctime(join(cwd, file))
+		#std9 = (datetime.datetime.fromtimestamp(mtime) + datetime.timedelta(hours = -9)).isoformat()
+		std9 = file[:8].split('.')[0].split('_')[0].split('-')[0]+'T'
+		print (std9)
+		if std9.split('T')[0] == pdr:
+			continue
+		if time.get(std9.split('T')[0], []) == []:
+			time[std9.split('T')[0]] = []
+		time[std9.split('T')[0]].append(file)
+	for tim in time.keys():
+		try:
+			mkdir(join(cwd, tim))
+		except Exception as ex:
+			print (ex.args)
+		for file in time[tim]:
+			src = join(cwd, file)
+			dst = join(cwd, join(tim, file))
+			shutil.move(src, dst)
 def runbatch(root, cwd, pdr, pngexp, ratio):
-    print(cwd)
     # Export PNG with scalebar
     s = gwy.gwy_app_settings_get()
     s['/module/pixmap/title_type'] = 0
@@ -28,10 +52,7 @@ def runbatch(root, cwd, pdr, pngexp, ratio):
     # ... (*lots* of possible settings, see ~/.gwyddion/settings)
 
     Files_To_Open = [ f for f in listdir(cwd) if isfile(join(cwd,f)) ]
-    
-    print(Files_To_Open)
-    if len(Files_To_Open) == 0:
-        return 'No files'
+
     try:
         mkdir(join(cwd,'Processed'))
     except Exception as sym:
@@ -41,10 +62,6 @@ def runbatch(root, cwd, pdr, pngexp, ratio):
         print (Files_To_Open)
     #Load first file to use as Merged file
     for filename in Files_To_Open:
-        if not filename[-3:][-1].isdigit():
-            Files_To_Open.remove(filename)
-            continue
-            
         print(filename)
         try:
             Temp = gwy.gwy_file_load(join(cwd,filename), RUN_NONINTERACTIVE)
@@ -53,7 +70,6 @@ def runbatch(root, cwd, pdr, pngexp, ratio):
                 print('right type')
                 Cont_Dest = Temp
                 Files_To_Open.remove(filename)
-                Fstfilename = filename
                 break
             Files_To_Open.remove(filename)
             print('loadedbutnot')
@@ -61,12 +77,8 @@ def runbatch(root, cwd, pdr, pngexp, ratio):
             print('except')
             print ("not proper file"+str(sym)+"\n")
             continue
-        #Add into current browser and Make Visible on display
-    try:
-        gwy.gwy_app_data_browser_add(Cont_Dest)
-    except Exception as ex:
-        print(ex.args)
-        return 'no veeco files to load'
+	#Add into current browser and Make Visible on display
+    gwy.gwy_app_data_browser_add(Cont_Dest)
     Cont_Dest.set_boolean_by_name('/0/data/visible', 1)
     print (Files_To_Open)
     #File Merge
@@ -75,14 +87,10 @@ def runbatch(root, cwd, pdr, pngexp, ratio):
     for key in DataFields.keys():
         title = Cont_Dest.get_string_by_name(key+"/title")
         if (title == 'Amplitude') : Cont_Dest.remove_by_prefix('/'+key.split('/')[1]+'/')
-        Cont_Dest.set_string_by_name(key+'/title', title+'.'+Fstfilename)
+        Cont_Dest.set_string_by_name(key+'/title', title+'.'+Files_To_Open[0])
 
 	#Rest of Containers
-    print ('Rest of Files',Files_To_Open)
     for filename in Files_To_Open :
-        if not filename[-3:][-1].isdigit():
-            Files_To_Open.remove(filename)
-            continue
         #print (orgfile, join(cwd,filename))
         try:
             Temp_Source = gwy.gwy_file_load(join(cwd,filename), RUN_NONINTERACTIVE)
@@ -99,22 +107,14 @@ def runbatch(root, cwd, pdr, pngexp, ratio):
             ID = key.split('/')[1]
             title = Cont_Source.get_string_by_name(key+"/title")
             if (title == 'Height') :
-            	   Cont_Source.set_string_by_name(key+'/title', title+'.'+filename)
-            	   gwy.gwy_app_data_browser_copy_channel(Cont_Source, int(ID), Cont_Dest)
-            	   print (key, title)
-    try:
-        gwy_app_data_browser_remove(Cont_Source)
-        del Cont_Source
-        print (gc.collect())
-    except Exception as ex:
-        print (ex.args)
-        
-    try:
-        mkdir(join(pngexp, root.split('/')[-1]))
-    except :
-        pass
+                Cont_Source.set_string_by_name(key+'/title', title+'.'+filename)
+                gwy.gwy_app_data_browser_copy_channel(Cont_Source, int(ID), Cont_Dest)
+                print (key, title)
+            gwy_app_data_browser_remove(Cont_Source)
+            del Cont_Source
+            print (gc.collect())
 
-    meta = open(join(pngexp, root.split('/')[-1])+'/'+'meta_file.txt' , 'a')
+
 	#Change Palette, Flatten, Correct line, Remove Scars, Change Scale
     DataFields = gwyutils.get_data_fields_dir(Cont_Dest)
     for key in DataFields.keys():
@@ -146,58 +146,39 @@ def runbatch(root, cwd, pdr, pngexp, ratio):
         colorr = Cont_Dest.get_int32_by_name('/'+ID+'/base/range-type')
         #Change_Color Palette
         Cont_Dest.set_string_by_name('/'+ID+'/base/palette', 'Gold')
+        
+        
         #Get Height Distribution and get Percentile color set range
         #Get CDH
         histogram = gwy.DataLine(1, 1, False)
-        DataFields[key].dh(histogram, 512)
-        median = histogram.get_max()
+        DataFields[key].cdh(histogram, 512)
         data = histogram.get_data()
-        ind_med = [i for i,v in enumerate(data) if data[i] == median][0]
         #Get Percentile Range	
-        cumhistogram = gwy.DataLine(1, 1, False)
-        DataFields[key].cdh(cumhistogram, 512)
-        data = cumhistogram.get_data()
-        Data_Range = DataFields[key].get_min_max()
-        Hist_pct = [0, 0]
-        Hist_pct[0] =  [float(index)/512 for index in range(ind_med, 0, -1) if (data[index] >= ratio[0] and data[index-1] <= ratio[0])][0]
-        Hist_pct[1] =  [float(index)/512 for index in range(ind_med, 512, 1)  if (data[index] <= ratio[1] and data[index+1] >= ratio[1])][0]
-        Range = Data_Range[1]-Data_Range[0]
-        Color_Range = {'min': Data_Range[0]+Range*Hist_pct[0], 'max':Data_Range[0]+Range*Hist_pct[1]}
-        Cont_Dest.set_boolean_by_name('/'+ID+'/data/visible', 1)
-        Cont_Dest.set_int32_by_name('/'+ID+'/base/range-type' , 1)
-        Cont_Dest.set_double_by_name('/'+ID+'/base/min', Color_Range['min'])
-        Cont_Dest.set_double_by_name('/'+ID+'/base/max', Color_Range['max'])
-        meta.write('Filename : '+ cwd+ '/' +str(title.split('Height.')[1])+'\n')
-        meta.write('ColorRange : '+'{min : \t%e,\t max : \t%e }\n' %(Color_Range['min'] ,Color_Range['max']))
-        #Change Color Range into (Full:0, Manual:1, Auto:2, Adaptive:3)
-        #Cont_Dest.set_int32_by_name('/'+ID+'/base/range-type', 2)
-        print (title)
         
-        gwy.gwy_file_save(Cont_Dest, join(pngexp, root.split('/')[-1])+ '/' +str(title)+'%d.png' % int(ID), gwy.RUN_NONINTERACTIVE)
+        Data_Range = DataFields[key].get_min_max()
+        Histogram_pct = [(float(index))/512 for index, value in enumerate(data) if (data[index] >= ratio[1] and data[index-1] <= ratio[1]) or (data[index] <= ratio[0] and data[index+1] >= ratio[0])]
+        Range = Data_Range[1]-Data_Range[0]
+        Color_Range = {'min': Data_Range[0]+Range*Histogram_pct[0], 'max':Data_Range[0]+Range*Histogram_pct[1]}
+        Cont_Dest.set_int32_by_name('/0/base/range-type' , 1)
+        Cont_Dest.set_double_by_name('/0/base/min', Color_Range['min'])
+        Cont_Dest.set_double_by_name('/0/base/max', Color_Range['max'])
+        
+        
+        #Change Color Range into (Full:0, Manual:1, Auto:2, Adaptive:3)
+        Cont_Dest.set_int32_by_name('/'+ID+'/base/range-type', 2)
+        print (title)
+        gwy.gwy_file_save(Cont_Dest, Tobe_Saved+'/'+str(title)+'%d.png' % int(ID), gwy.RUN_NONINTERACTIVE)
         Cont_Dest.set_boolean_by_name('/'+ID+'/data/visible', 0)
-    try:
-        gwy.gwy_file_save(Cont_Dest,join(cwd, 'Processed')+'/'+root.split('/')[-1]+pdr+'.gwy', gwy.RUN_NONINTERACTIVE)
-    except Exception as ex:
-        print(ex.args)
+    gwy.gwy_file_save(Cont_Dest,Tobe_Saved+'/'+filename_save+'.gwy', gwy.RUN_NONINTERACTIVE)
     gwy_app_data_browser_remove(Cont_Dest)
-    del Cont_Dest
-    print (gc.collect())
-    meta.close()
+
 
 def scan_dir_runbatch(root, cwd, pdr, pngexp, ratio):
 	
     #####Go into the deepest folder
     dirs = [ f for f in listdir(cwd) if isdir(join(cwd,f)) ]
-    try:
-        dirs.remove('Processed')
-    except :
-        pass
     #dirs = {'./' : Files}
     for dr in dirs:
         scan_dir_runbatch(root, join(cwd, dr), dr, pngexp, ratio)
     runbatch(root, cwd, pdr, pngexp, ratio)
-##########Get list of sub directories and run recursive self function
-
-#custgwy.sep_files(root, cwd, 'DDX - postexp')
-
-scan_dir_runbatch(root, cwd, cwd.split('/')[-1], pngexp, ratio)
+ 
